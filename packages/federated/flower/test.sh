0269153ad74981f9f41023c01721de89818cef04
Ken O'Brien <ken.obrien@amd.com>
2025-11-26 14:17:46 +0000

    flower progress with demo

--- packages/federated/flower/test.sh
+++ packages/federated/flower/test.sh
@@ -3,4 +3,4 @@
 # Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: MIT
 
-python -c "import flwr;print(flwr.__version__)"
+python -c "import flwr;print('flwr version:', flwr.__version__)"
